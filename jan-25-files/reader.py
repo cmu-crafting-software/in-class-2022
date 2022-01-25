f186f3fbe9cf9d313a1554ab3fdbfb939c0c294d
Dnae-ferguson <32795626+Dnae-ferguson@users.noreply.github.com>
2022-01-25 17:08:32 -0500

    Updated, average president age and created an array of birth state

--- jan-25-files/reader.py
+++ jan-25-files/reader.py
@@ -5,17 +5,32 @@ def presidentAge():
     f = open('presidents.json')
     presidents = json.load(f)
 
+    ages = []
     for president in presidents :
         birthYear = datetime.strptime(president['DOB'], '%Y-%m-%d')
-        print birthYear.year
-
-    ages = []
-    for president in presidents:
-        birthYear = datetime.strptime(president['DOB'], '%Y-%m-%d')
         DOB = birthYear.year
+        #print DOB
         age = 2022 - DOB
+        #print age
         ages.append(age) 
-    avgAge = mean(ages)
-    print(avgAge)
+
+    avgAge = sum(ages) / len(ages)
+    print "Average  president age: ", avgAge
+
+state = []
+def sortByState():
+    f = open('presidents.json')
+    presidents = json.load(f)
+
+    
+    for president in presidents :
+        birthPlace = (president['State'])
+        #print birthPlace
+        state.append(birthPlace)
+    print state
+        
+
+
 
 presidentAge()
+sortByState()
