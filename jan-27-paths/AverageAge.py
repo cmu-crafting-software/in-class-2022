f29b70e946d7ef43b676783436af6df7502d7029
LukaQiu <yincqiu@gmail.com>
2022-01-27 16:21:03 -0500

    Added Average Age & birthState in JSON file

---
+++ jan-27-paths/AverageAge.py
@@ -0,0 +1,19 @@
+import json
+from datetime import datetime
+
+def step3():
+    f = open("presidents.json")
+    presidents = json.load(f)
+
+    ages = []
+    for president in presidents :
+        dob = datetime.strptime(president['DOB'], '%Y-%m-%d')
+        #birthState = president['BirthState']
+        today = datetime.today()
+        age = today.year - dob.year
+        if (today.month, today.day) <  (dob.month, dob.day) :
+            age = age - 1
+        ages.append(age)
+    return ages
+
+print(step3())
