3a814bbc7c3e2a39b3719cea1a924aacf3659116
SMa2021 <80334667+SMa2021@users.noreply.github.com>
2022-01-27 16:51:29 -0500

    step 6 done

--- jan-27-paths/step5&6.py
+++ jan-27-paths/step5&6.py
@@ -1,22 +1,39 @@
 import glob
 import os
-
+from datetime import datetime
+import json
 
 def step5():
     print(os.getcwd())
     os.chdir('musicians/rock')
-    files = os.listdir()
+    files = os.listdir(os.getcwd())
     for f in files:
         print(f)
 
-def computeAverageAge(str[] paths):
+def computeAverageAge(paths):
     ages = []
-    
+    today = datetime.today()
+    for path in paths:
+        f = open(path)
+        people = json.load(f)
+        for person in people :
+            dob = datetime.strptime(person['DOB'], '%Y-%m-%d')
+            age = today.year - dob.year
+            if (today.month, today.day) <  (dob.month, dob.day) :
+                age = age - 1
+            ages.append(age)
+
+    return sum(ages)/len(ages)
 
 
-def step6():
-    print(os.getcwd())
-    files = glob.glob('**/*.csv', recursive=True)
+
+def step6(cwd):
+    os.chdir(cwd)
+    files = glob.glob('**/*.json', recursive=True)
     print(files)
+    print(computeAverageAge(files))
 
+
+cwd = os.getcwd()
 step5()
+step6(cwd)
