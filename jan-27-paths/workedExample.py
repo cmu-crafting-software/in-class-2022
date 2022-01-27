2d4b3ac30766f1103f05e4d69ffa9b27ec699aec
unknown <dcasalnu@andrew.cmu.edu>
2022-01-27 16:19:06 -0500

    edited .json file and started editing python file to sort by state. Python file not complete

--- jan-27-paths/workedExample.py
+++ jan-27-paths/workedExample.py
@@ -1,3 +1,31 @@
+from builtins import print
 import glob
+
+import json
+from datetime import datetime
+
+def step3():
+    f = open("presidents.json")
+    presidents = json.load(f)
+
+    SOB=[]
+    ages = []
+    name=[]
+    for president in presidents :
+        dob = datetime.strptime(president['DOB'], '%Y-%m-%d')
+        today = datetime.today()
+        age = today.year - dob.year
+        if (today.month, today.day) <  (dob.month, dob.day) :
+            age = age - 1
+        ages.append(age)
+        SOB.append(president['StateOfBirth'])
+        name.append(president['Last'])
+    return [SOB, name]
+
+list =step3()
+list.sort(1)
+print(list)
+
+
 files = glob.glob('**/*.csv', recursive=True)
-print(files)
+#print(files)
