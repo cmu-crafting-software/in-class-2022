af834d609c36ee0214604582df2eada9e823811d
jackj26 <jackj@andrew.cmu.edu>
2022-02-01 16:24:53 -0500

    Jack and Darren. Started building the code to compare a user input to secret word for wordle

--- feb-1-design/wordle.py
+++ feb-1-design/wordle.py
@@ -10,7 +10,16 @@
 #Get User Input
 
 #Check User Input against randomly generated word
+def comparison(user_guess,true_word) :
+    for letter in user_guess:
+        if user_guess[letter] == true_word[letter]:
+            print('_')
+        elif user_guess[letter] in true_word:
+            print('*')
+        else:
+            print('^')
 
+comparison('ideal','diner')
 #Check word against dictionary
 
 #Provide feedback to User 
