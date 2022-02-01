9d78b55e883d0c9ef894c47cd1b148f57fce4399
SMa2021 <80334667+SMa2021@users.noreply.github.com>
2022-02-01 16:25:01 -0500

    Yincheng and Shilin

--- feb-1-design/wordle.py
+++ feb-1-design/wordle.py
@@ -3,10 +3,42 @@
 # build a dictionary (5 letter words only)
 # generate a random word from the dictionary
 # get user input (limit to 5 letters each line)
+from locale import THOUSEP
+
+
+def getUserInput():
+    guess = input('Enter your guess: ')
+    return guess
     # prompt user to do another line for a total of 6 lines
+
 # ? convert letters to numbers so that it's easier for comparison
 # compare the input and the chosen word letter by letter and generate feedback
-    # check if input is in dictionary
-    # check which letters are used in the chosen word
-    # check if the used letters are in the right position
+    # check if it's in dictionary
+"""
+inputs:   guess, the user's input, assume it's valid
+          correct_word, the correct word
+outputs:  a string of character of length 5
+            _ represents the letter is not in the word
+            * means the letter is in the correct word but not in the right position
+            ^ means the letter is in the correct word and at the right position
+"""          
+def compareTwoWords(guess, correct_word):
+    output = ""
+    for i in range(len(guess)):
+        # check which letters are used in the chosen word
+        if correct_word.count(guess[i])==0:
+            # the letter is not in the word
+            output += "_"
+         # check if the used letters are in the right position
+        elif guess[i] == correct_word[i]:
+            output += "^"
+        else:
+            output += "*"
+    return output
+   
 # testing
+
+guess = getUserInput()
+print(compareTwoWords(guess,'aaaaa'))
+print(compareTwoWords('aaple','aaaaa'))
+print(compareTwoWords('apple','baaaa'))
