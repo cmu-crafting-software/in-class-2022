583406bdba2f6b492f1478703c2b01694a20f1d4
SMa2021 <80334667+SMa2021@users.noreply.github.com>
2022-02-01 15:50:21 -0500

    outline

---
+++ feb-1-design/wordle.py
@@ -0,0 +1,12 @@
+# Yincheng and Shilin
+
+# build a dictionary (5 letter words only)
+# generate a random word from the dictionary
+# get user input (limit to 5 letters each line)
+    # prompt user to do another line for a total of 6 lines
+# ? convert letters to numbers so that it's easier for comparison
+# compare the input and the chosen word letter by letter and generate feedback
+    # check if input is in dictionary
+    # check which letters are used in the chosen word
+    # check if the used letters are in the right position
+# testing
