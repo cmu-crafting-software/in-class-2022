13b58e6e7183e182d9b79c88576a5c73e34cb815
taryni <taryni@stanford.edu>
2022-02-01 15:59:16 -0500

    commented outlin of code with Dom.

---
+++ feb-1-design/wordle.py
@@ -0,0 +1,23 @@
+
+
+
+#Get a dictionary of words
+#Pick a word (random)
+#Query the user for the quess
+#Counter for user guesses
+
+
+#Comparator
+    #Check if user guess is a word
+        #If not, notify user
+    #Break word into list of strings
+    #Check if letter is right and in the right position
+    #Check if the user guessed letter are present at all
+    #Check if user guessed letter is present but in the wrong spot
+
+#Notify user of outcome and provide feedback
+#Loop from comparator until user either guesses word or runs out of guesses
+
+
+
+
