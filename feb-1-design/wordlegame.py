bfff96b972247ead5837eff6f6d28b9cf962f618
Dnae-ferguson <32795626+Dnae-ferguson@users.noreply.github.com>
2022-02-01 16:23:51 -0500

    D'Nae Lionel, checks first position only

--- feb-1-design/wordlegame.py
+++ feb-1-design/wordlegame.py
@@ -1,15 +1,29 @@
 #D'Nae Ferguson Lionel Sam
 
-#Pick a word and store it
+#Create a csv file of 5 letter words
+
+# Randomly pick a word 
+word = 'thorn'
 
 #Some DS that holds a list of words 5 letters long
 
 #Get user input 
+guess = input("Guess a word, it must be 5 letters long: ")
+
     # bounded to only 5 letters
     # should be an actual word
     # break after 6 guesses
 
 #Compare user input to stored word
+def compareFirstPosition():
+    if guess[0] == word[0]:
+        print("*")
+    elif (guess[0] == word[1]) | (guess[0] == word[2]) | (guess[0] == word[3]) | (guess[0] == word[4]):
+        print("^")
+    else: 
+        print("_")
+
+compareFirstPosition()
 
 # Some type of graphic interface for user to see what's happening
 
