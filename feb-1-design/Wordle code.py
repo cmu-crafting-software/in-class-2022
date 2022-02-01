089e4265602eb48fa577fee8ab72acd20dc26081
morganchen <morganc@andrew.cmu.edu>
2022-02-01 16:25:22 -0500

    Christine and Morgan Wordle code draft

---
+++ feb-1-design/Wordle code.py
@@ -0,0 +1,31 @@
+#Christine, Morgan
+
+#make a dictionary of words
+    #manually input words at first
+
+#get input from user
+#how to store the input words from user
+#how to compare the input with the Word
+#input: apple - correct word, table - input word
+#output: _ is totally wrong; * is right letter, right pos, ^ is right letter, wrong pos
+guess = "table"
+word = "apple"
+def compare():
+    for i in range(0,len(guess),1):
+        if guess[i] == word[i]:
+            print('*')
+        else if guess[i] != word[i]:
+            for i in range(0,len(guess),1):
+                
+
+
+    
+
+compare()
+    
+
+#show feedback
+#save correct letters in correct positions
+#get input from user
+#repeat
+
