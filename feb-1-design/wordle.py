0459bbc878be751f1340155cc4df55a517165b95
lviornery <lviornery@hmc.edu>
2022-02-01 16:29:45 -0500

    finished game

--- feb-1-design/wordle.py
+++ feb-1-design/wordle.py
@@ -1,19 +1,51 @@
-#initialize directory
+#Luis Viornery and Dan Connolly
+
+import random
+
+def compWord(guess,word):
+    comp = ['x']*len(guess)
+    for i in range(len(guess)):
+        if guess[i] == word[i]:
+            comp[i] = '!'
+        elif guess[i] in word:
+            comp[i] = 'o'
+    return comp
+    
+def getWord(wordList):
+    badGuess = True
+    while(badGuess):
+        guess = ''.join(filter(str.isalpha,input('Guess a five-letter word: '))).lower()
+        if (len(guess) == 5 and ''.join(guess) in wordList):
+            badGuess = False
+        else:
+            print('Bad input!')
+    return guess
 
 #initialize game state
 
+f = open('sgb-words.txt')
+flist = f.read().split('\n')
 #select target word
+word = random.choice(flist)
+f.close()
+maxTries = 6
+win = False
+for i in range(maxTries):
+    #get user input
+    guess = getWord(flist)
 
-#get user input
+    #compare user input to target word
+    comp = compWord(guess,word)
 
-#validate user input
-
-#compare user input to target word
-
-#give user feedback
-
-#update game state
-
-#end game (win)
-
-#end game (lose)
+    #give user feedback
+    print(guess)
+    print(''.join(comp))
+    if word == guess:
+        win = True
+        break
+if win:
+    #end game (win)
+    print('your winner!')
+else:
+    #end game (lose)
+    print('haha scrub')
