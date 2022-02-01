eee88ab22dcadf31ef6fc6add300a8b10be42a55
taryni <taryni@stanford.edu>
2022-02-01 16:26:51 -0500

    started writing function to compare guess word to secret word for wordle

--- feb-1-design/wordle.py
+++ feb-1-design/wordle.py
@@ -1,4 +1,4 @@
-
+# in class 2-1-22 Taryn and Dominic 
 
 
 #Get a dictionary of words
@@ -6,14 +6,35 @@
 #Query the user for the quess
 #Counter for user guesses
 
+secret = "beach"
+guess = "check"
+
+
+
+#Check if user guess is a word
+    #If not, notify user
 
 #Comparator
-    #Check if user guess is a word
-        #If not, notify user
-    #Break word into list of strings
+    
     #Check if letter is right and in the right position
     #Check if the user guessed letter are present at all
     #Check if user guessed letter is present but in the wrong spot
+def compare(sword, gword):
+    sword = sword.lower()
+    gword = gword.lower()
+    result = []
+
+    if sword == gword: print("Congrats! You won Wordle!")
+
+    for i in range(0,4,1):
+        for j in range(0,4,1):
+            if gword[i] == secret[j] and i == j: result.append("*")
+
+
+    
+    
+    print("_ = totally incorrect, * = right letter in right place; ^ right letter, wrong position")
+    return 
 
 #Notify user of outcome and provide feedback
 #Loop from comparator until user either guesses word or runs out of guesses
