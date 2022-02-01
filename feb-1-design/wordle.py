95003b9552139b52a0c0039a05b4f7df33c7e85a
lviornery <lviornery@hmc.edu>
2022-02-01 15:48:24 -0500

    made list

---
+++ feb-1-design/wordle.py
@@ -0,0 +1,19 @@
+#initialize directory
+
+#initialize game state
+
+#select target word
+
+#get user input
+
+#validate user input
+
+#compare user input to target word
+
+#give user feedback
+
+#update game state
+
+#end game (win)
+
+#end game (lose)
