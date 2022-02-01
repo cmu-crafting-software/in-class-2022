d6a62ed63d32e3d6f7345b089b5a17d6ca0556da
Nikole Chetty <nchetty@andrew.cmu.edu>
2022-02-01 15:50:15 -0500

    Psuedo-code for wordle

---
+++ feb-1-design/wordle.py
@@ -0,0 +1,15 @@
+# Nikole and Katie 
+
+# Import dictionary with english words
+# Get subset of dict with 5 letter words 
+# Generate a five letter word 
+
+# Loop 6 times: 
+    # Get keyboard input from user 
+    # Confirm input is 5 letters
+    # Convert input to lowercase/upper case (dependent on dict?)
+    # Check if input is a word 
+    # Compare user input string to generated string by index 
+    # Generate if statement that tells the user inputed entry matches generated word 
+    # Break if all correct 
+
--- jan-27-paths/avgAgeJsonCsv.py
+++ jan-27-paths/avgAgeJsonCsv.py
@@ -48,3 +48,5 @@ def step7():
 
 
 step7()
+
+# need to remove duplicates
