4370027204bcb53de73806bbaa95cf391acac28f
Christine <ctroller@andrew.cmu.edu>
2022-02-10 16:09:21 -0500

    inclassjan27

---
+++ jan-27-paths/ctrollerInClassExercise.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import json
+from datetime import datetime
+
+
+def step3():
+    f = open("presidents.json")
+    presidents = json.load(f)
+
+    ages = []
+    for president in presidents :
+        dob = datetime.strptime(president['DOB'], '%Y-%m-%d')
+        today = datetime.today()
+        age = today.year - dob.year
+        if (today.month, today.day) <  (dob.month, dob.day) :
+            age = age - 1
+        ages.append(age)
+    return ages
+
+def addnew():
+    f = open("presidents.json")
+    presidents = json.load(f)
+    presidents.append({'First': 'Abe', 'Last': 'Lincoln', 'DOB': '1809-02-12'})
+    
+
+    x = {'BirthState': 'anywhere'}
+
+    presidents.update(x)
+    print(presidents)
+
+addnew()
+
